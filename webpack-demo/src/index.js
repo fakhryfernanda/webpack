899a08eb6586fa9eaedbd4ae41e6e96372085478
fakhryfernanda <mff023@gmail.com>
2022-05-15 22:14:02 +0700

    Cleanup and prepare for output management

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,23 +1,10 @@
 // Lodash, now imported by this script
 import _ from 'lodash';
-import './style.css';
-import beluga from './beluga.jpg';
-import Data from './sample.xml';
-import Notes from './sample.csv';
 
 function component() {
     const element = document.createElement('div');
   
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-
-    // Add the image to our existing div.
-    const myIcon = new Image();
-    myIcon.src = beluga;
-    element.appendChild(myIcon);
-
-    console.log(Data);
-    console.log(Notes);
      
     return element;
   }
