8adbabc37d55f9f20e6330b60f7ed5d1c24e49c0
fakhryfernanda <mff023@gmail.com>
2022-05-15 21:44:44 +0700

    Bundling css and image with webpack

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,11 +1,19 @@
 // Lodash, now imported by this script
 import _ from 'lodash';
+import './style.css';
+import beluga from './beluga.jpg';
 
 function component() {
     const element = document.createElement('div');
   
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
+    element.classList.add('hello');
+
+    // Add the image to our existing div.
+    const myIcon = new Image();
+    myIcon.src = beluga;
+    element.appendChild(myIcon);
+     
     return element;
   }
   
